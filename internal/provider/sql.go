d8f03b676349b87466804aed242d89c7a6a2ff34
Artem Shahov <shahov9ad@gmail.com>
2024-12-17 09:52:59 +0300

    sql func

--- internal/provider/sql.go
+++ internal/provider/sql.go
@@ -2,7 +2,11 @@ package provider
 
 import "github.com/ValeryBMSTU/web-rk2/internal/entities"
 
-func (p *Provider) GetQuestion() (*entities. error {
+func (p *Provider) GetQuestion() (*entities.Question, error) {
+
+}
+
+func (p *Provider) UpdateScore(int) error {
 
 }
 
--- internal/usecase/interface.go
+++ internal/usecase/interface.go
@@ -1,15 +1,10 @@
 package usecase
 
-import "../internal/entities"
+import "github.com/ValeryBMSTU/web-rk2/internal/entities"
 
 type Provider interface {
-	InsertUser(entities.User) (*entities.User, error)
-	SelectAllUsers() ([]*entities.User, error)
+	GetQuestion() (*entities.Question, error)
 
-	SelectUserByID(id int) (*entities.User, error)
-	SelectUserByName(name string) (*entities.User, error)
-	SelectUserByEmail(name string) (*entities.User, error)
-
-	UpdateUserByID(id int, user entities.User) (*entities.User, error)
-	DeleteUserByID(id int) error
+	UpdateScore(int) error
+	GetScore() (int, error)
 }
