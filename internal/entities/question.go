e9251ea6aa5500edf5d5e63dab7415f49409b94d
Artem Shahov <shahov9ad@gmail.com>
2024-12-17 10:08:19 +0300

    true question

--- internal/entities/question.go
+++ internal/entities/question.go
@@ -2,10 +2,10 @@ package entities
 
 type Question struct {
 	ID       int    `json:"id,omitempty"`
-	QUESTION string `json:"question"`
-	ANS1     string `json:"ans1"`
-	ANS2     string `json:"ans2"`
-	ANS3     string `json:"ans3"`
-	ANS4     string `json:"ans4"`
-	POINTS   int    `json:"points"` //номер ответа
+	Question string `json:"question"`
+	Ans1     string `json:"ans1"`
+	Ans2     string `json:"ans2"`
+	Ans3     string `json:"ans3"`
+	Ans4     string `json:"ans4"`
+	Points   int    `json:"points"` //номер ответа
 }
